5631b549f6d4db0e9354df440f83c9d686525855
Antonio Mortari <113060294+AntonioMortari@users.noreply.github.com>
2023-09-11 08:24:43 -0300

    update

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const users = axios.create({
-    baseURL:' http://localhost:3000',
+    //  use esse se estiver rodando localmente -> baseURL:' http://localhost:3000',
+    baseURL:'https://goals-8h4u.onrender.com',
 })
 
 export default users
